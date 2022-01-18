34c0049523f6ebd902658a8c7b88d509762b1f17
Alessio0-110 <95098174+Alessio0-110@users.noreply.github.com>
2022-01-18 13:35:15 +0100

    Esercizio 1 e 4

--- 001_invertire_numero.py
+++ 001_invertire_numero.py
@@ -2,12 +2,13 @@
 In questo esercizio si richiede di invertire un dato numero intero, es: 1234 diventa 4321.
 Aspettarsi che l'input della funzione da definire sia già un numero intero n.
 """
-
-def inverti_numero(n):
-    pass
-
-assert inverti_numero(0) == 0
-assert inverti_numero(1234) == 4321
-assert inverti_numero(1200) == 21
-
-print("Se sei arrivato qui, congratulazioni!")
+x= input
+def inverti_numero (x):
+    x= input ("inserisci un numero: ")
+    x=  list(str(x))
+    
+    str1="".join(x)
+    leggi_x = len(str1)
+    inverti_x = str1[leggi_x::-1]
+    print(inverti_x)
+inverti_numero(x)
--- 004_poligoni_regolari.py
+++ 004_poligoni_regolari.py
@@ -5,24 +5,31 @@ Ritornare None nel caso di valori di input non validi.
 Aspettarsi che l'input della funzione siano due interi.
 Dopo gli assert, si pone un loop di input utente per testare la funzione. Si inserisca un'espressione non valida per terminare il loop.
 """
-
-def poligono_regolare(lunghezza, numero):
-    pass
-
-assert poligono_regolare(5, 4)==25
-assert poligono_regolare(-1, 4)==None
-assert poligono_regolare(5, 0)==None
-assert poligono_regolare(0, 0)==None
-
-print("Se sei arrivato qui, congratulazioni!\n\n")
+import math
+print ("Calcolatore area e perimetro di un poligono regolare.")
 
 while True:
-    l = int(input("Quanto è lungo il lato: "))
-    n = int(input("Quanti lati ci sono: "))
-    res = poligono_regolare(l, n)
-    if res==None:
-        print("Espressione non valida, termino il programma.")
-        break
-    else:
-        perimetro, area = res
-        print("Perimetro:", perimetro, ", Area:", area, "\n\n")
+    numero = int(input ("Inserisci il numero dei lati: "))
+    if numero < 3 or numero > 18 :
+            print("Inserire un numero di lati compreso tra 3 e 18.")
+            break
+    lunghezza_lato = int(input ("Inserisci la lunghezza del lato: "))
+    if lunghezza_lato == 0:
+            print("Inserire una lunghezza valida.")
+            break
+    
+
+    def poligono_regolare(lunghezza_lato, numero):
+        
+        numero_fisso =  [0.289, 0.5, 0.688, 0.866, 1.038, 1.207, 1.374, 1.539, 1.703, 1.866, 2.029, 2.191, 2.352, 2.514, 2.675, 2.836, 2.996, 3.157]
+        
+        numero_fisso= numero_fisso[lunghezza_lato-3] 
+        apotema = numero_fisso * lunghezza_lato
+        
+        perimetro = lunghezza_lato * numero
+        area = perimetro * apotema / ( 2 )
+        
+        print ("Perimetro: ",perimetro)
+        print ("Area: ", area)
+    poligono_regolare(lunghezza_lato,numero)
+    break
